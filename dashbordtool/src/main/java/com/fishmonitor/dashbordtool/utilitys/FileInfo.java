2ae4da78bd8b854749208c35eab6609d8157d6ab
vijay gopal reddy <vijaygopal.jetty@machint.com>
2024-02-19 23:44:40 -0500

    Added docker file with some of required changes

--- dashbordtool/src/main/java/com/fishmonitor/dashbordtool/utilitys/FileInfo.java
+++ dashbordtool/src/main/java/com/fishmonitor/dashbordtool/utilitys/FileInfo.java
@@ -14,13 +14,13 @@ public class FileInfo {
 	public List<?> getFilePath(String sourcePath ,String refPath) {
 		List<Map<String,Object>> videoList = new ArrayList<Map<String,Object>>();
 		try {
-			HashMap<String,Object> videoInfo= new HashMap<String, Object>();
 			Path videopath = Paths.get(sourcePath);
 			if (Files.notExists(videopath) || !Files.isDirectory(videopath)) {
 				System.out.println("File are not exist");
 			}
 			DirectoryStream<Path> directoryStream = Files.newDirectoryStream(videopath,"*.{mp4,avi,flv,mov,wmv}");
 				for (Path filePath : directoryStream) {
+					HashMap<String,Object> videoInfo= new HashMap<String, Object>();
 					videoInfo.put("name",filePath.getFileName().toString());
 					videoInfo.put("url", refPath+"/"+filePath.getFileName().toString());
 					videoList.add(videoInfo);
